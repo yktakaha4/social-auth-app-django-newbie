98f3aff301bb71417785191d8c96fa5e4bfb0026
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-04-28 14:48:28 +0900

    update わかりやすいイメージを追加

--- config/middleware.py
+++ config/middleware.py
@@ -1,4 +1,5 @@
 from social_django.middleware import SocialAuthExceptionMiddleware
+from social_core.exceptions import AuthCanceled
 from urllib.parse import quote
 
 
@@ -7,7 +8,16 @@ class CustomizedSocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
         # 以下に基づき実装
         # DEBUG=Trueの時に動作しなくなるので注意
         # https://python-social-auth.readthedocs.io/en/latest/configuration/django.html?highlight=error#exceptions-middleware
+        REDIRECT_URL = "/"
 
-        error_message = f"{type(exception).__name__}: {exception}"
+        if isinstance(exception, AuthCanceled):
+            # キャンセルはエラーではないのでメッセージは表示しない
+            print("detect AuthCanceled.")
+            error_message = None
+        else:
+            error_message = f"{type(exception).__name__}: {exception}"
 
-        return f"/?error_message={quote(error_message)}"
+        if error_message:
+            return f"{REDIRECT_URL}?error_message={quote(error_message)}"
+        else:
+            return REDIRECT_URL
