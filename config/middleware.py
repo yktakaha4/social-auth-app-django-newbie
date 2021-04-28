533e81e23cd6f79730f824b2c9f136cc224de7e9
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-04-28 14:40:38 +0900

    add comment

--- config/middleware.py
+++ config/middleware.py
@@ -4,6 +4,10 @@ from urllib.parse import quote
 
 class CustomizedSocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
     def get_redirect_uri(self, request, exception):
+        # 以下に基づき実装
+        # DEBUG=Trueの時に動作しなくなるので注意
+        # https://python-social-auth.readthedocs.io/en/latest/configuration/django.html?highlight=error#exceptions-middleware
+
         error_message = f"{type(exception).__name__}: {exception}"
 
         return f"/?error_message={quote(error_message)}"
